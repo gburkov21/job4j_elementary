aef0b1c57a02593112216bfc3e22c27f7e422af0
g.burkov <g.burkov@cheb.eraga.net>
2022-01-03 14:48:42 +0300

    5.7. Ипотека [#156315 #246440]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        double creditDebt = amount + (amount * (percent / 100.0));
-        while (salary < creditDebt) {
-            creditDebt -= salary;
+        int year = 0;
+        while (amount > 0) {
+            amount = (amount + amount * (percent / 100.0)) - salary;
             year++;
         }
         return year;
