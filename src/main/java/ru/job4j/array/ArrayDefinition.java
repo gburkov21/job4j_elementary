b951954f0ad4e3be4e54d30c0e60991957fcc365
g.burkov <g.burkov@cheb.eraga.net>
2022-01-04 08:32:15 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "John";
+        names[1] = "David";
+        names[2] = "Antony";
+        names[3] = "Michael";
+        for (String name : names) {
+            System.out.println(name);
+        }
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
